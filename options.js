f389747a3ace5b1aabe87d15b5d782e20285c435
Manu Garg <manugarg@gmail.com>
2012-08-07 17:05:38 +0530

    Fix licence header

--- options.js
+++ options.js
@@ -12,13 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
 
-/**
  * @author manugarg@gmail.com (Manu Garg)
- */
+ *
 
-/*
  * Directive for JSLint, so that it doesn't complain about these names not being
  * defined.
  */
