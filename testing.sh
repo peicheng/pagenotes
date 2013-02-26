e91950d69cc74fca3f9b6c891822048a8be40d1e
Manu Garg <manugarg@gmail.com>
2013-02-26 23:41:37 +0530

    = Description cannot be more 132 workds. Fix it. = Make testing script Mac compatible.

--- testing.sh
+++ testing.sh
@@ -33,8 +33,8 @@ git archive -o $baseVersionDir/1.tar.gz release-$base_version
 cd $baseVersionDir
 tar xzf 1.tar.gz; rm 1.tar.gz
 # Modify version and title
-sed -i -e "s/$base_version/$testing_version1/" manifest.json
-sed -i -e "s/Page Notes/Page Notes (testing)/" manifest.json
+sed -i "" -e "s/$base_version/$testing_version1/" manifest.json
+sed -i "" -e "s/Page Notes/Page Notes (testing)/" manifest.json
 zip -r ../cpn-base.zip .
 cd -
 
@@ -44,7 +44,7 @@ cd $testVersionDir
 tar xzf 1.tar.gz; rm 1.tar.gz
 
 # Modify version and title
-sed -i -e "s/\(.*\"version\"\: \"\)[[:digit:].]*\(.*\)/\1$testing_version2\2/" manifest.json
-sed -i -e "s/Page Notes/Page Notes (testing)/" manifest.json
+sed -i "" -e "s/\(.*\"version\"\: \"\)[[:digit:].]*\(.*\)/\1$testing_version2\2/" manifest.json
+sed -i "" -e "s/Page Notes/Page Notes (testing)/" manifest.json
 zip -r ../cpn-test.zip .
 cd -
